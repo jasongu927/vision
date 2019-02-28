8422cebd994543d3623863b8140c3024f22e4fd3
jasongu927 <45615504+jasongu927@users.noreply.github.com>
2019-02-27 19:58:31 -0500

    Update metrics.h

--- project2/src/metrics.h
+++ project2/src/metrics.h
@@ -14,8 +14,23 @@ typedef struct{
 	char name[100];
 	int metric;
 }img_metric;
+
+typedef struct{
+	int red_hist[255];
+	int blue_hist[255];
+	int green_hist[255];
+	bool is_grayscale;
+	int total_pixels;
+}color_hist;
  
 bool compare(const img_metric &a, const img_metric &b);
 
 int SSD(cv::Mat &comp, cv::Mat &source, int N);
 
+color_hist* make_hist(bool is_grayscale);
+
+color_hist* make_hist_from_image(cv::Mat& src);
+
+color_hist* compare_hists(color_hist* src, color_hist* compare);
+
+int hist_ssd(color_hist* src);
