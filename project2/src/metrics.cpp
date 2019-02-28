a1281736a964dc6fdfaec0c25fb3d27b2b1acd09
jasongu927 <45615504+jasongu927@users.noreply.github.com>
2019-02-27 19:56:35 -0500

    Update metrics.cpp

--- project2/src/metrics.cpp
+++ project2/src/metrics.cpp
@@ -47,4 +47,49 @@ int SSD(cv::Mat &comp, cv::Mat &source, int N){
 	return ssd;
 }
 
+//extensions
+// use our own histograms instead of OpenCV ones
+
+
+color_hist* make_hist(bool is_grayscale){
+	color_hist* hist = new(color_hist);
+	hist->is_grayscale = is_grayscale;
+	for(int i = 0; i < 255; i++){
+		hist -> red_hist[i] = 0;
+		hist -> blue_hist[i] = 0;
+		hist -> green_hist[i] = 0;
+	}
+	return hist;
+}
+
+color_hist* make_hist_from_image(cv::Mat& src){
+	color_hist* hist = make_hist(true);
+	for(auto it = src.begin<cv::Vec3b>(); it != src.end<cv::Vec3b>(); ++it){
+		hist->blue_hist[(*it)[0]]++; //bgr
+		hist->green_hist[(*it)[1]]++;
+		hist->red_hist[(*it)[2]]++;
+		hist->total_pixels++;
+	}
+	return hist;
+}
+
+color_hist* compare_hists(color_hist* src, color_hist* compare){
+	color_hist* result = make_hist(false);
+	for(int i = 0; i < 255; i++){
+		result -> red_hist[i] = src -> red_hist[i] - compare -> red_hist[i];
+		result -> blue_hist[i] = src -> blue_hist[i] - compare -> blue_hist[i];
+		result -> green_hist[i] = src -> green_hist[i] - compare -> green_hist[i];
+	}
+	return result;
+}
+
+int hist_ssd(color_hist* src){
+	int sum = 0;
+	for(int i = 0; i < 255; i++){
+		sum += src -> red_hist[i] * src -> red_hist[i];
+		sum += src -> green_hist[i] * src -> green_hist[i];
+		sum += src -> blue_hist[i] * src -> blue_hist[i];
+	}
+	return sum;
+}
 
